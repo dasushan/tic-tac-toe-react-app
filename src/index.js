bbba6a1d390c1c066345df43bbe8f70055df21c5
dasushan <53913896+dasushan@users.noreply.github.com>
2023-04-30 04:13:20 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {console.log("current environment = ", process.env)}
     <App />
   </React.StrictMode>
 );
